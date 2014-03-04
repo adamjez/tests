71fbab35dd3cb79c1b1f4e3e65e693503a59bc9f
Adam Jez <spiritakcz@gmail.com>
2014-03-04 17:26:36 +0100

    Pridan test do ipk kontroluje jen return code

---
+++ ipk/proj1/test.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+green='\e[0;32m' 
+endGreen='\e[0m'
+
+red='\e[0;31m' 
+endRed='\e[0m'
+
+succes=0
+
+
+declare -a argument=('ftp://anonymous:secret@ftp.fit.vutbr.cz:21/pub/systems/centos' 'ftp.ubuntu.com/ubuntu/dists/' 'ftp.fit.vutbr.cz/' 'ftp.linux.cz/pub/local/' 'ftp://atrey.karlin.mff.cuni.cz/pub/' 'ftp://ftp.eunet.cz/' 'ftp://ftp.muni.cz/pub' 'ftp://anonymous:heslo%3a@ftp.fit.vutbr.cz');
+
+
+echo "Spoustim testy" > logfile
+
+for i in "${argument[@]}"
+do
+   echo "Spoustim ftpclient s argumentem: ${i}" | tee -a logfile 1>&1
+   ./ftpclient "${i}" >> logfile
+
+	if [ $? -ne 0 ]; then 
+		echo -e "${red}Neuspech${endRed}"
+		printf "Neuspec\n\n" >> logfile
+	else
+		((succes++));
+		echo -e "${green}Uspech${endGreen}"
+		printf "Uspech\n\n" >> logfile 
+	fi
+done
+
+
+echo "VYSLEDKY TESTU:" | tee -a logfile 1>&1
+echo "Uspesne probehlo ${succes} / ${#argument[@]} spojeni" | tee -a logfile 1>&1
+
+
+
+exit 0
