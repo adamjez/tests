4e8db9c141694908034b62d14a123291dc7ef809
Adam Jez <spiritakcz@gmail.com>
2014-03-05 14:19:03 +0100

    Dalsi test 24 - z dokumentacniho prikladu

--- ipp/proj1/_stud_tests.sh
+++ ipp/proj1/_stud_tests.sh
@@ -39,7 +39,7 @@ endGreen='\e[0m'
 red='\e[0;31m' 
 endRed='\e[0m'
 
-declare -a tests=(test01 test02 test03 test04 test05 test06 test07 test08 test09 test10 test11 test12 test13 test14 test15 test16 test17 test18 test19 test20 test21 test22);
+declare -a tests=(test01 test02 test03 test04 test05 test06 test07 test08 test09 test10 test11 test12 test13 test14 test15 test16 test17 test18 test19 test20 test21 test22 test23 test24);
 declare -A params
 
 for TEST in "${tests[@]}"
